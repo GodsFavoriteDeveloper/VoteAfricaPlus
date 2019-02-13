e5965b0cdd8c73bb4907b58b1218a2cf45f40fa1
Norman Dube <ndubewebdesigner@gmail.com>
2019-02-13 13:52:59 +0200

    Updated Official Page UI

--- src/pages/official/official.ts
+++ src/pages/official/official.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the OfficialPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,6 +8,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'official.html',
 })
 export class OfficialPage {
+  official: any = 'feed'
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
